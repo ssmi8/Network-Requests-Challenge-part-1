88eb28bbe939ce79bff9dce71f895aab8aab0cca
ssmi8 <smithsteve8@hotmail.com>
2022-12-07 16:20:03 +0000

    Challenge complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
-import css from './App.module.css';
+import css from "./App.module.css";
+// import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
-import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from "./components/NavBarForm";
-import Content from './components/Content';
-import ContentHooks from './components/ContentHooks';
+//import Content from "./components/Content";
+// import ContentHooks from "./components/ContentHooks";
+// import ContentAPI from "./components/ContentAPI";
+import ContentAPIHooks from "./components/ContentAPIHooks";
 
 function App() {
   return (
@@ -12,7 +14,9 @@ function App() {
       <NavBarForm />
       <Sidebar />
       {/* <Content /> */}
-      <ContentHooks />
+      {/* <ContentHooks /> */}
+      {/* <ContentAPI /> */}
+      <ContentAPIHooks />
     </div>
   );
 }
